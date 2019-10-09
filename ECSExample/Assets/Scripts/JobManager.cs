dc58f0cd553d56b5070e7efd5c1986e15cb39fb8
Proboszcz777 <watroba.grzegorz@gmail.com>
2019-10-09 15:39:33 +0200

    Projects unity update, ECS Setup

---
+++ ECSExample/Assets/Scripts/JobManager.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+using UnityEngine.Jobs;
+
+public class JobManager : Singleton<JobManager>
+{
+    private TransformAccessArray taa_mobs;
+    private NativeList<Vector3> nl_startPos;
+    private NativeList<Vector3> nl_targetPos;
+    private NativeArray<MobState> na_mobStates;
+
+    JobHandle movementJH;
+
+    struct SetupRaycastJob : IJobParallelForTransform
+    {
+        [ReadOnly]
+        public int layerMask;
+
+        [ReadOnly]
+        public Vector3 wallPos;
+
+        [ReadOnly]
+        public NativeList<Vector3> targetPos;
+
+        [WriteOnly]
+        public NativeArray<RaycastCommand> rayCommands;
+
+        public void Execute(int index, TransformAccess transform)
+        {
+            Vector3 shootTarget = new Vector3(targetPos[index].x, wallPos.y, targetPos[index].z);
+            Vector3 dir = shootTarget - transform.position;
+            rayCommands[index] = new RaycastCommand(transform.position, dir, SettingsManager.ShootingRange, layerMask);
+        }
+    }
+
+    struct MobMovementJob : IJobParallelForTransform
+    {
+        [ReadOnly]
+        public float deltaTime;
+
+        [ReadOnly]
+        public NativeList<Vector3> startPos;
+
+        [ReadOnly]
+        public NativeList<Vector3> targetPos;
+
+        [ReadOnly]
+        public NativeArray<RaycastHit> hits;
+
+        public NativeArray<MobState> mobStates;
+
+        public void Execute(int index, TransformAccess transform)
+        {
+            Vector3 curPos = transform.position;
+
+            if (mobStates[index] == MobState.ToTarget && hits[index].normal != Vector3.zero)
+                mobStates[index] = MobState.Throw;
+
+            switch (mobStates[index])
+            {
+                case MobState.ToTarget:
+                    {
+                        curPos = Vector3.MoveTowards(curPos, targetPos[index], deltaTime);
+
+                        if (Vector3.Distance(curPos, targetPos[index]) < 2.0f)
+                            mobStates[index] = MobState.FromTarget;
+                    }
+                    break;
+
+                case MobState.FromTarget:
+                    {
+                        curPos = Vector3.MoveTowards(curPos, startPos[index], deltaTime);
+
+                        if (Vector3.Distance(curPos, startPos[index]) < 2.0f)
+                            mobStates[index] = MobState.ToTarget;
+                    }
+                    break;
+            };
+
+            transform.position = curPos;
+        }
+    }
+
+    void Start() {}
+
+    private void OnDestroy()
+    {
+        taa_mobs.Dispose();
+        nl_startPos.Dispose();
+        nl_targetPos.Dispose();
+        na_mobStates.Dispose();
+    }
+
+    private void Update()
+    {
+        if (!taa_mobs.isCreated || taa_mobs.length == 0 ||
+                nl_startPos.Length != taa_mobs.length ||
+                nl_targetPos.Length != taa_mobs.length ||
+                na_mobStates.Length != taa_mobs.length)
+            return;
+
+        NativeArray<RaycastCommand> na_rayCommands = new NativeArray<RaycastCommand>(taa_mobs.length, Allocator.TempJob);
+        NativeArray<RaycastHit> na_rayHits = new NativeArray<RaycastHit>(taa_mobs.length, Allocator.TempJob);
+
+        var setupRaycastJob = new SetupRaycastJob
+        {
+            layerMask = 1 << LayerMask.NameToLayer("Wall"),
+            rayCommands = na_rayCommands,
+            targetPos = nl_targetPos,
+            wallPos = GameManager.instance.Target.transform.position
+        };
+
+        var setupJH = setupRaycastJob.Schedule(taa_mobs);
+        var raycastJH = RaycastCommand.ScheduleBatch(na_rayCommands, na_rayHits, 32, setupJH);
+
+        var mobMovementJob = new MobMovementJob
+        {
+            deltaTime = Time.deltaTime,
+            hits = na_rayHits,
+            mobStates = na_mobStates,
+            startPos = nl_startPos,
+            targetPos = nl_targetPos
+        };
+
+        movementJH = mobMovementJob.Schedule(taa_mobs, raycastJH);
+
+        movementJH.Complete();
+
+        for (int i = 0; i < na_mobStates.Length; ++i)
+        {
+            if (na_mobStates[i] == MobState.Throw)
+            {
+                SpearBehavior spear = PoolManager.instance.SpearPool.SpawnObject(taa_mobs[i].position + SettingsManager.ThrowingPoint,
+                                                                                     Quaternion.Euler(SettingsManager.ThrowingRotation)) as SpearBehavior;
+
+                spear.Throw();
+                na_mobStates[i] = MobState.FromTarget;
+            }
+        }
+
+        na_rayCommands.Dispose();
+        na_rayHits.Dispose();
+    }
+
+    internal void AddMobsToSystem(int mobCnt)
+    {
+        movementJH.Complete();
+
+        if (!taa_mobs.isCreated)
+            taa_mobs = new TransformAccessArray(0);
+
+        if (na_mobStates == null || !na_mobStates.IsCreated)
+            na_mobStates = new NativeArray<MobState>(0, Allocator.Persistent);
+
+        if (!nl_startPos.IsCreated)
+            nl_startPos = new NativeList<Vector3>(Allocator.Persistent);
+
+        if (!nl_targetPos.IsCreated)
+            nl_targetPos = new NativeList<Vector3>(Allocator.Persistent);
+
+        int oldMobCnt = PoolManager.instance.MobPool.m_cnt - mobCnt;
+
+       for (int i = 0; i < mobCnt; ++i)
+        {
+            var startPos = GameManager.instance.GetSpawnPosFromStart(GameManager.instance.m_defaultSpawnPos, oldMobCnt + i, 2.0f);
+            var SpawnedMob = PoolManager.instance.MobPool.SpawnObject(startPos, Quaternion.identity) as MobFightComponent;
+            taa_mobs.Add(SpawnedMob.transform);
+
+            nl_startPos.Add(startPos);
+            nl_targetPos.Add(new Vector3(startPos.x, startPos.y, GameManager.instance.Target.transform.position.z));
+        }
+
+        if (na_mobStates.Length > 0)
+        {
+            var tempMobState = new NativeArray<MobState>(na_mobStates, Allocator.Temp);
+            na_mobStates.Dispose();
+            na_mobStates = new NativeArray<MobState>(tempMobState.Length + mobCnt, Allocator.Persistent);
+            NativeArray<MobState>.Copy(tempMobState, 0, na_mobStates, 0, tempMobState.Length);
+        }
+        else
+        {
+            na_mobStates.Dispose();
+            na_mobStates = new NativeArray<MobState>(mobCnt, Allocator.Persistent);
+        }
+
+        for (int i = oldMobCnt; i < na_mobStates.Length; ++i)
+            na_mobStates[i] = MobState.ToTarget;
+
+        if (PoolManager.instance.SpearPool.m_cnt < PoolManager.instance.MobPool.m_cnt * 3)
+            AddSpearsToSystem(mobCnt * 3);
+    }
+
+    internal void RemoveMobsFromSystem(int mobCnt)
+    {
+        movementJH.Complete();
+
+        for (int i = 0; i < mobCnt; ++i)
+        {
+            if (taa_mobs.length > 0)
+                taa_mobs.RemoveAtSwapBack(taa_mobs.length - 1);
+
+            if (nl_startPos.Length > 0)
+                nl_startPos.RemoveAtSwapBack(nl_startPos.Length - 1);
+
+            if (nl_targetPos.Length > 0)
+                nl_targetPos.RemoveAtSwapBack(nl_targetPos.Length - 1);
+        }
+
+        int maxChange = mobCnt;
+        if (maxChange > na_mobStates.Length)
+        {
+            na_mobStates.Dispose();
+            na_mobStates = new NativeArray<MobState>(0, Allocator.Persistent);
+        }
+        else
+        {
+            int newSize = na_mobStates.Length - mobCnt;
+            var tempStates = new NativeArray<MobState>(newSize, Allocator.Temp);
+            NativeArray<MobState>.Copy(na_mobStates, 0, tempStates, 0, newSize);
+            na_mobStates.Dispose();
+            na_mobStates = new NativeArray<MobState>(newSize, Allocator.Persistent);
+            tempStates.CopyTo(na_mobStates);
+        }
+
+        PoolManager.instance.MobPool.Expand(-mobCnt);
+
+        RemoveSpearsFromSystem(mobCnt * 3);
+    }
+
+    internal void AddSpearsToSystem(int spearCnt)
+    {
+        PoolManager.instance.SpearPool.Expand(spearCnt);
+    }
+
+    internal void RemoveSpearsFromSystem(int spearCnt)
+    {
+        PoolManager.instance.SpearPool.Expand(-spearCnt);
+    }
+}
---
+++ ECSExample/Assets/Scripts/PoolManager.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : Singleton<PoolManager>
+{
+    [Serializable]
+    public class ObjectPool
+    {
+        public bool IsExpandable = true;
+        public CustomBehaviour m_template;
+        public int m_cnt = 0;
+        private List<CustomBehaviour> m_pool = new List<CustomBehaviour>();
+        private List<bool> m_used = new List<bool>();
+
+        public CustomBehaviour GetAt(int i)
+        {
+            if (i < 0 || i > m_cnt)
+                return null;
+
+            return m_pool[i];
+        }
+
+        public void Init()
+        {
+            AddInstances(m_cnt);
+        }
+
+        public void Expand(int i_cnt)
+        {
+            m_cnt += i_cnt;
+
+            int changeAmount = i_cnt;
+
+            if (m_cnt < 0)
+            {
+                changeAmount = Math.Abs(i_cnt) + m_cnt;
+                m_cnt = 0;
+            }
+
+            if (i_cnt > 0)
+                AddInstances(changeAmount);
+            else
+                RemoveInstances(changeAmount);
+        }
+
+        private void AddInstances(int i_cnt)
+        {
+            for (int i = 0; i < i_cnt; ++i)
+            {
+                var pooledObj = Instantiate(m_template, PoolManager.instance.gameObject.transform);
+                pooledObj.gameObject.SetActive(false);
+                m_pool.Add(pooledObj);
+                m_used.Add(false);
+            }
+
+            UIManager.instance.RefreshPoolCount();
+        }
+
+        private void RemoveInstances(int i_cnt)
+        {
+            int itemCnt = Math.Abs(i_cnt);
+
+            for (int i = 0; i < itemCnt; ++i)
+            {
+                m_pool[m_pool.Count - 1].StopAllCoroutines();
+                m_pool[m_pool.Count - 1].gameObject.SetActive(false);
+                GameObject.Destroy(m_pool[m_pool.Count - 1].gameObject);
+                m_used.RemoveAt(m_pool.Count - 1);
+                m_pool.RemoveAt(m_pool.Count - 1);
+            }
+
+            UIManager.instance.RefreshPoolCount();
+        }
+
+        public CustomBehaviour SpawnObject(Vector3 i_position, Quaternion i_rotation, Transform parent = null)
+        {
+            int index = m_used.IndexOf(false);
+            if (index >= 0 && index < m_pool.Count)
+            {
+                m_used[index] = true;
+                var objToReturn = m_pool[index];
+
+                if (parent != null)
+                    objToReturn.transform.SetParent(parent);
+
+                objToReturn.Restart();
+                objToReturn.transform.position = i_position;
+                objToReturn.transform.rotation = i_rotation;
+                objToReturn.gameObject.SetActive(true);
+                return objToReturn;
+            }
+            else
+            {
+                if (IsExpandable)
+                {
+                    Expand(1);
+                    return SpawnObject(i_position, i_rotation, parent);
+                }
+                else
+                {
+                    //Debug.LogWarning("Wrong index in object pool!");
+                }
+            }
+
+            return null;
+        }
+
+        public void ReturnToPool(CustomBehaviour i_obj)
+        {
+            i_obj.StopAllCoroutines();
+
+            if (i_obj.transform.parent != PoolManager.instance.gameObject.transform)
+                i_obj.transform.SetParent(PoolManager.instance.gameObject.transform);
+
+            i_obj.gameObject.SetActive(false);
+            int index = m_pool.IndexOf(i_obj);
+            if (index >= 0 && index < m_used.Count)
+            {
+                m_used[index] = false;
+            }
+            else
+            {
+                Debug.LogWarning("Returning to the wrong pool!");
+            }
+        }
+    }
+
+    public ObjectPool MobPool;
+    public ObjectPool SpearPool;
+
+    public void Start()
+    {
+        MobPool.Init();
+        SpearPool.Init();
+    }
+}
